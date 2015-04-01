8413bf0c6d8c65a424f15957cc6f00f71f9e55af
epahomov <pahomov.egor@gmail.com>
2015-04-01 16:57:26 +0300

    ---

--- spark-shell.sh
+++ spark-shell.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker pull epahomov/docker-spark
-run -i -t -P --link spark_master:spark_master epahomov/docker-spark /spark-shell.sh
+docker run -i -t -P --link spark_master:spark_master epahomov/docker-spark /spark-shell.sh
