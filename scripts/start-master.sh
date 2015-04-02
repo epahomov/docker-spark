dedbefc2b48832d0f04b2e0460abb85a557ad6c8
epahomov <pahomov.egor@gmail.com>
2015-04-02 18:21:28 +0300

    sfdsf

--- scripts/start-master.sh
+++ scripts/start-master.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+/remove_alias.sh
 export SPARK_MASTER_IP=`awk 'NR==1 {print $1}' /etc/hosts`
 export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
 /usr/local/spark/sbin/start-master.sh --properties-file /spark-defaults.conf -i $SPARK_LOCAL_IP
