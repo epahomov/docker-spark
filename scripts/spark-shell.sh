fdbdbf036d557361c4a78ecfd0b563c912a8a552
epahomov <pahomov.egor@gmail.com>
2015-04-02 19:32:49 +0300

    sdfsdf

--- scripts/spark-shell.sh
+++ scripts/spark-shell.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 /remove_alias.sh
 cd /usr/local/spark
-export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
+export SPARK_LOCAL_IP=0.0.0.0
 ./bin/spark-shell --master spark://${SPARK_MASTER_PORT_7077_TCP_ADDR}:${SPARK_MASTER_ENV_SPARK_MASTER_PORT} -i ${SPARK_LOCAL_IP}
--- scripts/start-master.sh
+++ scripts/start-master.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 /remove_alias.sh
-export SPARK_MASTER_IP=`awk 'NR==1 {print $1}' /etc/hosts`
-export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
+export SPARK_MASTER_IP=0.0.0.0
+export SPARK_LOCAL_IP=0.0.0.0
 /usr/local/spark/sbin/start-master.sh --properties-file /spark-defaults.conf -i $SPARK_LOCAL_IP
 /bin/bash
