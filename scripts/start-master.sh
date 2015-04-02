3e03c7a4b46e6ff30d394a89b0912e7876fdddf3
epahomov <pahomov.egor@gmail.com>
2015-04-02 14:48:43 +0300

    sfd

--- scripts/start-master.sh
+++ scripts/start-master.sh
@@ -2,4 +2,4 @@
 export SPARK_MASTER_IP=`awk 'NR==1 {print $1}' /etc/hosts`
 export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
 /usr/local/spark/sbin/start-master.sh --properties-file /spark-defaults.conf -i $SPARK_LOCAL_IP
-/bin/bash -i ${SPARK_LOCAL_IP}
+/bin/bash
